d552b54f14eaa61f521c117bd78df9e651a34ae8
umbobabo <umbertobabini@economist.com>
2018-02-08 14:39:34 +0000

    Added parts

---
+++ src/parts/customer-links.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { renderListOfLinks } from '../index';
+
+export default function CustomerLinks(links, config, LinkComponent, i13n) {
+  if (!links) {
+    return null;
+  }
+  return (
+    <div className="ec-footer__list ec-footer__list--subs">
+      <ul className="list">{renderListOfLinks(links, config, LinkComponent, i13n)}</ul>
+    </div>
+  );
+}
---
+++ src/parts/economist-links.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { renderListOfLinks } from '../index';
+
+export default function EconomistLinks(links, config, LinkComponent, i13n) {
+  if (!links) {
+    return null;
+  }
+  return (
+    <div className="ec-footer__list ec-footer__list--economist">
+      <ul className="list">{renderListOfLinks(links, {}, LinkComponent, i13n)}</ul>
+    </div>
+  );
+}
