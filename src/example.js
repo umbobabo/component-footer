ffe3cc6dfbc9d459c4442527eb899a8f967d0fee
umbobabo <umbertobabini@economist.com>
2018-06-07 17:20:07 +0100

    feat: single link overridable

--- src/example.js
+++ src/example.js
@@ -1,126 +1,142 @@
-import 'babel-polyfill';
-import React from 'react';
-import Footer from './';
+/* eslint-disable no-alert */
+import "babel-polyfill";
+import React from "react";
+import Footer from "./";
+
+function overridingComponent(props) {
+  function alertMe() {
+    alert("You can add custom functionality");
+  }
+  return (
+    <button onClick={alertMe} {...props}>
+      Overriden link
+    </button>
+  );
+}
 
 const links = {
   customer: [
     {
-      title: 'Subscribe',
-      href: 'http://www.economistgroupmedia.com',
+      title: "Subscribe",
+      href: "http://www.economistgroupmedia.com"
     },
     {
-      title: 'Contact us',
-      href: 'http://www.economist.com/rights/',
+      title: "Contact us",
+      href: "http://www.economist.com/rights/"
     },
     {
-      title: 'Help',
-      href: 'http://www.economist.com/rights/',
+      title: "Help",
+      href: "http://www.economist.com/rights/"
     },
     {
-      title: 'Open Future',
-      href: 'http://www.economist.com/openfuture/',
-    },
+      title: "Open Future",
+      href: "http://www.economist.com/openfuture/"
+    }
   ],
   economist: [
     {
-      title: 'Advertise',
-      href: 'products/subscribe',
+      title: "Advertise",
+      href: "products/subscribe"
     },
     {
-      title: 'Editorial Staff',
-      href: 'node/21013093',
+      title: "Editorial Staff",
+      href: "node/21013093"
     },
     {
-      title: 'Reprints',
-      href: 'node/21017280',
+      title: "Reprints",
+      href: "node/21017280"
     },
     {
-      title: 'Site Map',
-      href: 'node/21554326',
+      title: "Site Map",
+      href: "node/21554326"
     },
     {
-      title: 'Careers',
-      href: 'node/21013080',
-    },
+      title: "Careers",
+      href: "node/21013080"
+    }
   ],
   social: [
     {
-      title: 'Facebook',
-      meta: 'facebook',
-      href: 'https://www.economist.com',
-      internal: false,
+      title: "Facebook",
+      meta: "facebook",
+      href: "https://www.economist.com",
+      internal: false
     },
     {
-      title: 'Twitter',
-      meta: 'twitter',
-      href: 'https://www.economist.com',
-      internal: false,
+      title: "Twitter",
+      meta: "twitter",
+      href: "https://www.economist.com",
+      internal: false
     },
     {
-      title: 'Google',
-      meta: 'googleplus',
-      href: 'https://www.economist.com',
-      internal: false,
+      title: "Google",
+      meta: "googleplus",
+      href: "https://www.economist.com",
+      internal: false
     },
     {
-      title: 'Linkedin',
-      meta: 'linkedin',
-      href: 'https://www.economist.com',
-      internal: false,
+      title: "Linkedin",
+      meta: "linkedin",
+      href: "https://www.economist.com",
+      internal: false
     },
     {
-      title: 'Tumblr',
-      meta: 'tumblr',
-      href: 'https://www.economist.com',
-      internal: false,
+      title: "Tumblr",
+      meta: "tumblr",
+      href: "https://www.economist.com",
+      internal: false
     },
     {
-      title: 'Instagram',
-      meta: 'instagram',
-      href: 'https://www.economist.com',
-      internal: false,
+      title: "Instagram",
+      meta: "instagram",
+      href: "https://www.economist.com",
+      internal: false
     },
     {
-      title: 'Youtube',
-      meta: 'youtube',
-      href: 'https://www.economist.com',
-      internal: false,
+      title: "Youtube",
+      meta: "youtube",
+      href: "https://www.economist.com",
+      internal: false
     },
     {
-      title: 'rss',
-      meta: 'rss',
-      href: 'https://www.economist.com',
-      internal: false,
+      title: "rss",
+      meta: "rss",
+      href: "https://www.economist.com",
+      internal: false
     },
     {
-      title: 'mail',
-      meta: 'mail',
-      href: 'https://www.economist.com',
-      internal: true,
-    },
+      title: "mail",
+      meta: "mail",
+      href: "https://www.economist.com",
+      internal: true
+    }
   ],
   business: [
     {
-      title: 'Terms of Use',
-      href: 'node/21013093',
+      title: "Terms of Use",
+      href: "node/21013093"
+    },
+    [
+      {
+        title: "Privacy!!",
+        href: null
+      },
+      overridingComponent
+    ],
+    {
+      title: "Cookies",
+      href:
+        "http://www.economistgroup.com/results_and_governance/governance/privacy"
     },
     {
-      title: 'Privacy',
-      href: 'node/21554326',
-    },
-    {
-      title: 'Cookies',
-      href: 'http://www.economistgroup.com/results_and_governance/governance/privacy',
-    },
-    {
-      title: 'Accessibility',
-      href: 'http://www.economistgroup.com/results_and_governance/governance/privacy',
-    },
-  ],
+      title: "Accessibility",
+      href:
+        "http://www.economistgroup.com/results_and_governance/governance/privacy"
+    }
+  ]
 };
-const quote = 'Published since September 1843 to take part in <br/><em>“a severe ' +
-  'contest between intelligence, which presses forward,<br/>and an unworthy, timid ' +
-  'ignorance obstructing our progress.”</em>';
-export default (
-  <Footer data={links} quote={quote} />
-);
+const quote =
+  "Published since September 1843 to take part in <br/><em>“a severe " +
+  "contest between intelligence, which presses forward,<br/>and an unworthy, timid " +
+  "ignorance obstructing our progress.”</em>";
+export default <Footer data={links} quote={quote} />;
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,33 +1,31 @@
 /* eslint-disable id-match, camelcase */
 
-import React from 'react';
-import Icon from '@economist/component-icon';
-import slugger from 'slugger';
+import React from "react";
+import Icon from "@economist/component-icon";
+import slugger from "slugger";
 
-const iconSize = '48px';
+const iconSize = "48px";
 function createI13nModel({ title, href }, i13n, { position }) {
-  return ({
+  return {
     tedl: {
-      id: `${ slugger(title) }_footer-link`,
+      id: `${slugger(title)}_footer-link`,
       module_id: i13n ? i13n.module.id : null,
       position,
-      type: 'link',
+      type: "link",
       attributes: {
         name: title,
-        destination: href,
-      },
-    },
-  });
+        destination: href
+      }
+    }
+  };
 }
 
 export function createI13nProps(link, i13n, options) {
-  return i13n ?
-    { i13nModel: createI13nModel(link, i13n, options) } :
-    {};
+  return i13n ? { i13nModel: createI13nModel(link, i13n, options) } : {};
 }
 
 function createLinkTag(LinkComponent, i13n) {
-  let Link = LinkComponent ? LinkComponent : 'a';
+  let Link = LinkComponent ? LinkComponent : "a";
   if (i13n) {
     Link = i13n.I13nLink;
   }
@@ -35,82 +33,108 @@ function createLinkTag(LinkComponent, i13n) {
 }
 
 let i13nIndex = 1;
-function * getPosition() {
+function* getPosition() {
   while (i13nIndex) {
     yield i13nIndex++;
   }
 }
 
 const position = getPosition();
-export function renderListOfLinks(listOfLinks, {
-  useIcons = false,
-  iconColor = '#B6B6B6',
-} = {}, LinkComponent, i13n) {
+export function renderListOfLinks(
+  listOfLinks,
+  { useIcons = false, iconColor = "#B6B6B6" } = {},
+  LinkComponent,
+  i13n
+) {
   const i13nPosition = i13n ? position.next().value : null;
-  return Array.isArray(listOfLinks) && listOfLinks.map((link, index) => {
-    let linkContents = link.title;
-    if (useIcons) {
-      linkContents = <Icon icon={link.meta} color={iconColor} size={iconSize} />;
-    }
-    const Link = createLinkTag(LinkComponent, i13n);
-    const i13nProps = createI13nProps(link, i13n, { position: `${ i13nPosition }.${ index + 1 }` });
-    if (link.internal === false) {
+  return (
+    Array.isArray(listOfLinks) &&
+    listOfLinks.map((link, index) => {
+      let LinkRenderingComponent = LinkComponent;
+      if (Array.isArray(link)) {
+        // Override a specific LinkComponent.
+        LinkRenderingComponent = link[1];
+        // Use the first argument as original link information.
+        link = link[0];
+      }
+      let linkContents = link.title;
+      if (useIcons) {
+        linkContents = (
+          <Icon icon={link.meta} color={iconColor} size={iconSize} />
+        );
+      }
+      const Link = createLinkTag(LinkRenderingComponent, i13n);
+      const i13nProps = createI13nProps(link, i13n, {
+        position: `${i13nPosition}.${index + 1}`
+      });
+      if (link.internal === false) {
+        return (
+          <li className="list__item" key={index}>
+            <Link
+              className="ec-footer__link ec-footer__link--external"
+              href={link.href}
+              target="_blank"
+              {...i13nProps}
+            >
+              {linkContents}
+            </Link>
+          </li>
+        );
+      }
       return (
         <li className="list__item" key={index}>
           <Link
-            className="ec-footer__link ec-footer__link--external"
+            className="ec-footer__link"
             href={link.href}
-            target="_blank"
+            key={index}
             {...i13nProps}
           >
             {linkContents}
           </Link>
         </li>
       );
-    }
-    return (
-      <li className="list__item" key={index}>
-        <Link
-          className="ec-footer__link"
-          href={link.href}
-          key={index}
-          {...i13nProps}
-        >
-          {linkContents}
-        </Link>
-      </li>
-    );
-  });
+    })
+  );
 }
 
 export function targetIfNeeded({ internal }) {
   if (internal === false) {
-    return { target: '_blank' };
+    return { target: "_blank" };
   }
   return {};
 }
 
 export function renderSocialListContent(listOfLinks, LinkComponent, i13n) {
-  const allExceptMail = listOfLinks.filter(({ meta }) => meta !== 'mail');
-  return renderListOfLinks(allExceptMail, { useIcons: true }, LinkComponent, i13n);
+  const allExceptMail = listOfLinks.filter(({ meta }) => meta !== "mail");
+  return renderListOfLinks(
+    allExceptMail,
+    { useIcons: true },
+    LinkComponent,
+    i13n
+  );
 }
 
 export function renderNewsletterLink(social, LinkComponent, i13n) {
-  const newsletter = social.filter(({ meta }) => meta === 'mail')[0] || null;
+  const newsletter = social.filter(({ meta }) => meta === "mail")[0] || null;
   if (!newsletter) {
     return [];
   }
   const Link = createLinkTag(LinkComponent, i13n);
   const i13nPosition = position.next().value;
-  const i13nProps = createI13nProps(newsletter, i13n, { position: i13nPosition });
+  const i13nProps = createI13nProps(newsletter, i13n, {
+    position: i13nPosition
+  });
   return (
     <Link
       className="ec-footer__link ec-footer__subscribe-newsletter-link"
-      href={newsletter.href} {...targetIfNeeded(newsletter)}
+      href={newsletter.href}
+      {...targetIfNeeded(newsletter)}
       {...i13nProps}
     >
-      <Icon icon="mail"
-        className="ec-footer__subscribe-newsletter-icon" color="#B6B6B6"
+      <Icon
+        icon="mail"
+        className="ec-footer__subscribe-newsletter-icon"
+        color="#B6B6B6"
         size={iconSize}
       />
       {newsletter.title}
